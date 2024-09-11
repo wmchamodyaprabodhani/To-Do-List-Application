cde76907a1194a49ac5fe8b73e9ee03dabac825d
Chamodya Prabodhani <36436815-eyestruckgraphi@users.noreply.replit.com>
2024-09-11 21:40:10 +0000

    Your commit message

--- main.py
+++ main.py
@@ -1,23 +1,25 @@
 class ToDoApp:
-  def __init__(self):
-      self.tasks = []
 
-  def add_task(self, task):
-      self.tasks.append(task)
-      return f"Task '{task}' added!"
+    def __init__(self):
+        self.tasks = []
 
-  def view_tasks(self):
-      return self.tasks
+    def add_task(self, task):
+        self.tasks.append(task)
+        return f"Task '{task}' added!"
 
-  def complete_task(self, task):
-      if task in self.tasks:
-          self.tasks.remove(task)
-          return f"Task '{task}' completed!"
-      else:
-          return f"Task '{task}' not found!"
+    def view_tasks(self):
+        return self.tasks
+
+    def complete_task(self, task):
+        if task in self.tasks:
+            self.tasks.remove(task)
+            return f"Task '{task}' completed!"
+        else:
+            return f"Task '{task}' not found!"
+
 
 # Test the application
 if __name__ == "__main__":
-  app = ToDoApp()
-  app.add_task("Finish GitHub profile")
-  print(app.view_tasks())
+    app = ToDoApp()
+    app.add_task("Finish GitHub profile ASAP")
+    print(app.view_tasks())
